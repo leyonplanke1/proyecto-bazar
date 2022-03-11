1dfc32ea81881fc5ba12ada2b0c5ada4aff4587a
leyonplanke <i2623180@continental.edu.pe>
2022-03-10 19:32:28 -0500

    first commit

---
+++ back-end/laravel/app/Models/v1/Cliente.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\v1;
+
+use Illuminate\Database\Eloquent\Model;
+use BinaryCabin\LaravelUUID\Traits\HasUUID;
+
+
+class Producto extends Model
+{
+    use HasUUID;
+
+    protected $table = 'clientes';
+    protected $primaryKey ="id";
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $uuidFieldName = 'id';
+
+    
+}
